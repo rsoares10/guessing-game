e136e3de977ff925e9bf84d017c22be16d2bb878
rsoares10 <rsoares.monteiro10@gmail.com>
2023-02-20 15:35:36 -0300

    feat: add game welcome header

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("*********************************\n");
+    printf("* Welcome to our guessing game! *\n");
+    printf("*********************************\n");
+}
